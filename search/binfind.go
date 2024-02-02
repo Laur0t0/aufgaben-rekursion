568e9e6cee64109f9700eef1463ca9e658e1b923
Laur0t0 <corvin2505@icloud.com>
2024-02-02 15:51:22 +0100

    02.02.

--- search/binfind.go
+++ search/binfind.go
@@ -4,6 +4,22 @@ package search
 // Falls x nicht gefunden wird, wird -1 zurückgegeben.
 // Da die Liste sortiert ist, wird die binäre Suche verwendet.
 func FindSorted(list []int, x int) int {
-	// TODO
-	return -1
+	if len(list) == 0 {
+		return -1
+	}
+	c := len(list) / 2
+	if list[c] == x {
+		return c
+	}
+	sublist := list[:c]
+	offset := 0 // Offset für den Index in der Teilliste
+	if x > list[c] {
+		sublist = list[c+1:]
+		offset = c + 1
+	}
+	pos := FindSorted(sublist, x)
+	if pos == -1 {
+		return -1
+	}
+	return pos + offset
 }
