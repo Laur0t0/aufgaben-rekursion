b65b31215c7d95c44953118b0287f4346884b5f9
Laur0t0 <corvin2505@icloud.com>
2024-02-02 14:15:31 +0100

    02.02.

--- lists/compare.go
+++ lists/compare.go
@@ -4,6 +4,16 @@ package lists
 // Verwenden Sie Rekursion und benutzen Sie NICHT die len-Funktion.
 // Sie können die Hilfsfunktion Empty aus empty.go verwenden.
 func ListsEqual(list1, list2 []int) bool {
-	// TODO
-	return false
+
+	if Empty(list1) && Empty(list2) {
+		return true
+	}
+	if Empty(list1) && !Empty(list2) || !Empty(list1) && Empty(list2) {
+		return false
+	}
+	if list1[0] != list2[0] {
+		return false
+	}
+
+	return ListsEqual(list1[1:], list2[1:])	//führt die Funktion (also den Vergleich) ab der Stelle 1 (1:) aus
 }
