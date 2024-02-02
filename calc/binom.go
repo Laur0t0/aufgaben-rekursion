02e0dcb0893462a7700deab5b6ed609785310b31
Laur0t0 <corvin2505@icloud.com>
2024-02-02 12:54:57 +0100

    Update binom.go

--- calc/binom.go
+++ calc/binom.go
@@ -3,6 +3,6 @@ package calc
 // BinomialCoefficient erwartet zwei Zahlen n und k und liefert den
 // Binomialkoeffizienten "n über k".
 func BinomialCoefficient(n, k int) int {
-	// TODO
+	
 	return 0
 }
