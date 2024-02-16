c03587928adfa7aca55f7431e2cad1752660b364
Laur0t0 <corvin2505@icloud.com>
2024-02-16 12:30:07 +0100

    16.02.24

--- strings/chain.go
+++ strings/chain.go
@@ -3,6 +3,9 @@ package strings
 // Chain erwartet einen String und hängt ihn n mal hintereinander.
 // Liefert das Ergebnis.
 func Chain(s string, n int) string {
-	// TODO
-	return ""
+	var chain string
+	for i := 0; i < n; i++ {
+		chain += s
+	}
+	return chain
 }
--- strings/contains.go
+++ strings/contains.go
@@ -2,6 +2,6 @@ package strings
 
 // Contains prüft, ob der String s die Sequenz seq enthält.
 func Contains(s, seq string) bool {
-	// TODO
+	
 	return false
 }
