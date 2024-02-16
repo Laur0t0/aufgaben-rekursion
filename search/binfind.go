637f85e18aed626df7c332876bef5680e09c6542
Laur0t0 <corvin2505@icloud.com>
2024-02-16 11:10:27 +0100

    16.02.24

--- search/binfind.go
+++ search/binfind.go
@@ -4,22 +4,21 @@ package search
 // Falls x nicht gefunden wird, wird -1 zurückgegeben.
 // Da die Liste sortiert ist, wird die binäre Suche verwendet.
 func FindSorted(list []int, x int) int {
+	//Fall 1 (wenn Liste leer ist)
 	if len(list) == 0 {
 		return -1
 	}
 	c := len(list) / 2
+	//Fall 2 (wenn wert an mitte genau x)
 	if list[c] == x {
 		return c
 	}
 	sublist := list[:c]
 	offset := 0 // Offset für den Index in der Teilliste
+	//Fall 3 (wenn x größer als wert an mitte)
 	if x > list[c] {
 		sublist = list[c+1:]
 		offset = c + 1
 	}
-	pos := FindSorted(sublist, x)
-	if pos == -1 {
-		return -1
-	}
-	return pos + offset
+	return offset + FindSorted(sublist, x)
 }
--- search/find.go
+++ search/find.go
@@ -1,9 +1,24 @@
 package search
 
+import "github.com/tel23a-inf/aufgaben-rekursion/lists"
+
 // Find sucht in einer Liste nach dem ersten Vorkommen von x
 // und gibt dessen Index zurück. Falls x nicht gefunden wird,
 // wird -1 zurückgegeben.
 func Find(list []int, x int) int {
-	// TODO
-	return -1
+	// for i := 0; i < len(list); i++ {
+	// 	if list[i] == x {
+	// 		return i
+	// 	}
+	// }
+	// return -1
+
+	if lists.Empty(list) {
+		return -1
+	}
+
+	if list[0] != x {
+		return 1 + Find(list[1:], x)
+	}
+	return 0
 }
