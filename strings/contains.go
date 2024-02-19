e0b2318da2f1171d94bf162f56ea36046e43a7f9
Laur0t0 <corvin2505@icloud.com>
2024-02-19 19:38:34 +0100

    19.02.23

--- strings/contains.go
+++ strings/contains.go
@@ -2,6 +2,13 @@ package strings
 
 // Contains prüft, ob der String s die Sequenz seq enthält.
 func Contains(s, seq string) bool {
-	
-	return false
+	n := 0
+	m := n + len(seq)
+	if len(s) < len(seq) {
+		return false
+	}
+	if s[n:m] == seq {
+		return true
+	}
+	return Contains(s[1:], seq)
 }
